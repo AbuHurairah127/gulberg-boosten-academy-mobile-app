894f4100589becb0a66d3bca8a57f344edaa008b
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-09-20 22:52:51 +0500

    store folder copied from the web version not connected

--- App.tsx
+++ App.tsx
@@ -26,7 +26,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-
+import {Provider} from 'react';
 const Section: React.FC<
   PropsWithChildren<{
     title: string;
@@ -59,7 +59,7 @@ const Section: React.FC<
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
-
+  console.log('App');
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
@@ -79,8 +79,8 @@ const App = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+            Abu hurairah Edit <Text style={styles.highlight}>App.tsx</Text> to
+            change this screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
