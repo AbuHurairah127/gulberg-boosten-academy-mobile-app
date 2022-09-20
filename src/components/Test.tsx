7b9d1db85e90155fe67976e3a1b801d8e178c6ba
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-09-20 23:26:18 +0500

    redux successfully connected with typescript react native app

---
+++ src/components/Test.tsx
@@ -0,0 +1,18 @@
+import {View, Text} from 'react-native';
+import {AppState} from '../store/reducers/rootReducer';
+import React from 'react';
+import {useSelector} from 'react-redux';
+export default function Test() {
+  const isAuthenticated = useSelector(
+    (store: AppState) => store.authReducer.isAuthenticated,
+  );
+  console.log(
+    '🚀 ~ file: Test.js ~ line 9 ~ Test ~ isAuthenticated',
+    isAuthenticated,
+  );
+  return (
+    <View>
+      <Text>{isAuthenticated}</Text>
+    </View>
+  );
+}
