a007d63735969f1a5de3bf1e62c8317af6c3d6ef
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-09-24 21:57:42 +0500

    Initial commit
    Generated by create-react-native-app 3.8.0.

--- src/config/store.js
+++
@@ -1,5 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./../store/reducers/rootReducer";
-const store = createStore(rootReducer, applyMiddleware(thunk));
-export default store;
--- src/store/reducers/rootReducer.ts
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from 'redux';
-import authReducer from './authReducer';
-const rootReducer = combineReducers({
-  authReducer,
-});
-export type AppState = ReturnType<typeof rootReducer>;
-export default rootReducer;
