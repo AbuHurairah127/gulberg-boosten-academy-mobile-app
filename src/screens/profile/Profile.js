1d7d646c9f792202ce8af1560ef1e0310fcc1397
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-10-12 07:05:14 +0500

    pwofile pg lower section completed

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -1,14 +1,38 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import React from 'react';
 import {styles} from './ProfileStyles';
 import useProfile from './useProfile';
 import Button from '../../components/button/Button';
+import {useSelector} from 'react-redux';
 const Profile = () => {
+  const cUser = useSelector(store => store.authReducer.cUser);
+  console.log('🚀 ~ file: Profile.js ~ line 9 ~ Profile ~ cUser', cUser.gender);
   const {onLogoutHandler} = useProfile();
   return (
     <View style={styles.container}>
-      <View style={styles.upperSection}></View>
+      <View style={styles.upperSection}>
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.image}
+            source={require('./../../assets/male-icon.png')}
+          />
+        </View>
+      </View>
       <View style={styles.lowerSection}>
+        <View style={styles.infosContainer}>
+          <View style={styles.infoLabelContainer}>
+            <Text style={styles.infoLabel}>Name:</Text>
+            <Text style={styles.infoLabel}>Father's Name:</Text>
+            <Text style={styles.infoLabel}>Father's Phone:</Text>
+            <Text style={styles.infoLabel}>Student Roll No.:</Text>
+          </View>
+          <View style={styles.infoValContainer}>
+            <Text style={styles.infoVal}>{cUser.name}</Text>
+            <Text style={styles.infoVal}>{cUser.fatherName}</Text>
+            <Text style={styles.infoVal}>{cUser.fatherNum}</Text>
+            <Text style={styles.infoVal}>{cUser.rollNo}</Text>
+          </View>
+        </View>
         <View style={styles.logoutButtonContainer}>
           <Button label="Logout" event={onLogoutHandler} />
         </View>
--- src/screens/profile/ProfileStyles.js
+++ src/screens/profile/ProfileStyles.js
@@ -5,14 +5,34 @@ export const styles = StyleSheet.create({
   },
   upperSection: {
     flex: 1,
-    backgroundColor: 'red',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    // width: '100 %',
   },
   lowerSection: {
     flex: 1,
-    backgroundColor: 'green',
     justifyContent: 'space-around',
   },
+  infosContainer: {
+    margin: 15,
+    padding: 5,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    flex: 1,
+    flexDirection: 'row',
+  },
+  singleInfo: {
+    flexDirection: 'row',
+  },
+  infoLabelContainer: {flex: 1, justifyContent: 'space-evenly'},
+  infoLabel: {fontSize: 16, fontWeight: 'bold'},
+  infoValContainer: {flex: 1, justifyContent: 'space-evenly'},
+  infoVal: {fontSize: 16},
   logoutButtonContainer: {
     alignItems: 'center',
+    margin: 10,
   },
 });
