30700afe4e3a31aa40e5b76f278ebe1ee98ab9f8
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-11-01 20:10:58 +0500

    finalization

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native';
 import {Provider} from 'react-redux';
 import store from './src/config/store';
 import Toast from 'react-native-toast-message';
 import MainNavigation from './src/navigation/MainNavigation';
+import RNBootSplash from 'react-native-bootsplash';
+
 const App = () => {
+  useEffect(() => {
+    RNBootSplash.hide();
+  }, []);
   return (
     <Provider store={store}>
       <SafeAreaView
